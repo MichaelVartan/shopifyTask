0b958a07c91558252b9f2fb25f6e2be29fbc49df
MichaelVartan <vartan200143@gmail.com>
2023-06-17 19:56:47 +0300

    Update

--- assets/miniCartHeader.js
+++ assets/miniCartHeader.js
@@ -1,16 +1,19 @@
-(()=>{
-    const miniCart = document.querySelector("miniCart")  
-    async function getMiniCart() {
-        try {
-          const response = await fetch('https://jsonplaceholder.typicode.com/users');
-          if (response.status < 400) {
-            const users = await response.json();
-            displayUsers(users);
-          } else {
-            alert('Error from server');
-          }
-        } catch (err) {
-          alert(err.message);
-        }
-      }      
+(() => {
+	const miniCart = document.querySelector('miniCart');
+	async function getMiniCart() {
+		try {
+			const response = await fetch('?section_id=template-cart');
+			if (response.status < 400) {
+				const section = await response.json();
+				miniCart.innerHTML = section;
+			} else {
+				alert('Error from server');
+			}
+		} catch (err) {
+			alert(err.message);
+		}
+	}
+
+	const btn = document.querySelector('#miniCartBtn');
+	btn.addEventListener('click', getMiniCart);
 })();
